95a662ccc932fa732759579e85acaf98ae583779
talessgomes <tales.santos.gomes@outlook.com>
2023-09-20 22:07:20 -0300

    add a button to add 5 pokemon on website

---
+++ assets/js/poke-api.js
@@ -0,0 +1,34 @@
+const pokeApi = {};
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types; //i catch the firt type of pokemon
+
+    pokemon.types = types
+    pokemon.type = type //This is the first type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    
+    return pokemon // in this case, i go to return the details of pokeDetail in the class pokemon.
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequest) => Promise.all(detailRequest))
+    .then((pokemonsDetails) => pokemonsDetails)
+    .catch((error) => console.log(error))
+}
